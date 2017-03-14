7cd98c213947d4ad414afd4c591cdf2a654379ed
Yue Lyu <lyu.yue1992@gmail.com>
2017-03-14 13:05:54 -0400

    append fix

--- constants.h
+++ constants.h
@@ -1,20 +1,23 @@
-#define DATA_MSG_TYPE    1
-#define ACK_MSG_TYPE     2
-#define SEQ_MSG_TYPE     3
+#define DATA_MSG_TYPE       1
+#define ACK_MSG_TYPE        2
+#define SEQ_MSG_TYPE        3
+#define SEQ_ACK_MSG_TYPE    4
 
-#define MAX_MSG_COUNT   1000
-#define MAX_HOST        100
-#define MAX_PENDING     100
-#define MAX_DELAY       1
+#define MAX_MSG_COUNT       1000
+#define MAX_HOST            100
+#define MAX_PENDING         100
+#define MAX_DELAY           1
 
-#define MSG_SIZE        sizeof(struct Message)
-#define ACK_HEADER_SIZE sizeof(struct AckRecordHeader)
+#define MSG_SIZE            sizeof(struct Message)
+#define ACK_HEADER_SIZE     sizeof(struct AckRecordHeader)
 
-#define ACK_MSG_SIZE    sizeof(struct AckMessage)
-#define DATA_MSG_SIZE   sizeof(struct DataMessage)
-#define SEQ_MSG_SIZE    sizeof(struct SeqMessage)
+#define ACK_MSG_SIZE        sizeof(struct AckMessage)
+#define DATA_MSG_SIZE       sizeof(struct DataMessage)
+#define SEQ_MSG_SIZE        sizeof(struct SeqMessage)
+#define SEQ_ACK_MSG_SIZE    sizeof(struct SeqAckMessage)
 
-#define ENDLESS_LOOP    1
-#define SEND_FLAG       1
+#define ENDLESS_LOOP        1
+#define SEND_FLAG           1
+#define RELIABLE_THR        1500
 
-#define BUF_SIZE        512
+#define BUF_SIZE            512
